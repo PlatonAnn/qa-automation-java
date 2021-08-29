7bd07e27d7534de2940719f3e673352224b6dfc9
a.p.platon <a.p.platon@tinkoff.ru>
2021-08-29 19:13:11 +0300

    Add two tests (HomeWork №7)

--- src/main/java/com/tinkoff/edu/app/StaticVariableLoanCalcRepository.java
+++ src/main/java/com/tinkoff/edu/app/StaticVariableLoanCalcRepository.java
@@ -3,6 +3,13 @@ package com.tinkoff.edu.app;
 public class StaticVariableLoanCalcRepository implements LoanCalcRepository {
     private int requestId = 0;
 
+    public StaticVariableLoanCalcRepository() {
+
+    }
+    public StaticVariableLoanCalcRepository(int requestId) {
+        this.requestId = requestId;
+    }
+
     /**
      * Persists request
      * @param request
